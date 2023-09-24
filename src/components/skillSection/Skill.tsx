f80c7695807fe7c183d46f154a0b5d125bb082b1
Sujeong Ji <suji.sujeongji@gmail.com>
2023-09-24 09:25:37 +0900

    feat: add skills section

--- src/components/skillSection/Skill.tsx
+++ src/components/skillSection/Skill.tsx
@@ -1,10 +1,56 @@
 import React from "react";
 import styles from "./skill.module.css";
+import { BsCheckCircleFill } from "react-icons/bs";
 
 const Skill = () => {
+  const frontend = [
+    "JavaScript",
+    "TypeScript",
+    "React.js",
+    "Next.js",
+    "Webpack",
+    "Yarn Berry",
+  ];
+
+  const backend = ["Node.js", "Express.js", "MongoDB", "Firebase"];
+
   return (
     <section id="skill" className={styles.container}>
-      Skill
+      <h1 className={styles.title}>
+        My <span>Skills</span>
+      </h1>
+      <div className={styles.skills}>
+        <div className={styles.skillBox}>
+          <div>
+            <h1>Frontend Development</h1>
+          </div>
+          <div>
+            <ul>
+              {frontend.map((skill, index) => (
+                <li key={index}>
+                  <BsCheckCircleFill className={styles.icon} />
+                  {skill}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className={styles.skillBox}>
+          <div>
+            <h1>Backend Development</h1>
+          </div>
+          <div>
+            <ul>
+              {backend.map((skill, index) => (
+                <li key={index}>
+                  <BsCheckCircleFill className={styles.icon} />
+                  {skill}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
